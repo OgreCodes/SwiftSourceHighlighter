2810ea245ba6b22edeb92c8534cf2ee5ecee2eb6
Dennis Baker <dennis@biomedgps.com>
2017-02-28 23:42:27 -0800

    Initial Commit

---
+++ SwiftSourceHighlighter/SwiftSourceHighlighter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSourceHighlighter.h
+//  SwiftSourceHighlighter
+//
+//  Created by Dennis Baker on 2/28/17.
+//  Copyright © 2017 Dennis Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSourceHighlighter.
+FOUNDATION_EXPORT double SwiftSourceHighlighterVersionNumber;
+
+//! Project version string for SwiftSourceHighlighter.
+FOUNDATION_EXPORT const unsigned char SwiftSourceHighlighterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSourceHighlighter/PublicHeader.h>
+
+
